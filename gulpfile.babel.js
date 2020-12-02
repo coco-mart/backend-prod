99e6e553c1e4b3d7a83645075e0b2b557f1ff3e9
Pavithran Rajkumar <pavithranr.16cse@gmail.com>
2020-12-03 00:36:21 +0530

    Gulp config change

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,7 +7,7 @@ const plugins = gulpLoadPlugins();
 
 const paths = {
     js: ["./**/*.js", "!dist/**", "!node_modules/**", "!coverage/**"],
-    nonJs: ["./package.json", "./.gitignore"],
+    nonJs: ["./package.json", "./.gitignore", "./.env"],
     tests: "./server/tests/*.js",
 };
 
@@ -19,7 +19,10 @@ gulp.task("clean", (done) => {
 
 // Copy non-js files to dist
 gulp.task("copy", () =>
-    gulp.src(paths.nonJs).pipe(plugins.newer("dist")).pipe(gulp.dest("dist"))
+    gulp
+        .src(paths.nonJs, { allowEmpty: true })
+        .pipe(plugins.newer("dist"))
+        .pipe(gulp.dest("dist"))
 );
 
 // Compile ES6 to ES5 and copy to dist
