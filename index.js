7d2830c536f65024a547ee20b4983f267e28d94d
Pavithran Rajkumar <pavithranr.16cse@gmail.com>
2020-12-03 01:44:49 +0530

    Index change

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ const logger = loggers.get(config.loggerName); // eslint-disable-line no-global-
 // make bluebird default Promise
 Promise = require("bluebird"); // eslint-disable-line no-global-assign
 
-// module.parent check is required to support mocha watch
-if (!module.parent) {
-    // listen on port config.port
-    app.listen(config.port, () => {
-        logger.info(
-            `The application has started on port ${config.port} (${config.env})`
-        ); // eslint-disable-line no-console
-    });
-}
+// listen on port config.port
+app.listen(config.port, () => {
+    logger.info(
+        `The application has started on port ${config.port} (${config.env})`
+    ); // eslint-disable-line no-console
+});
 
 export default app;
