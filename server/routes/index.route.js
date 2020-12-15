c33bb938755ae95091d6df4f7be95431f7ae1fd2
Pavithran Rajkumar <pavithranr.16cse@gmail.com>
2020-12-16 01:16:38 +0530

    Added google places API

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./user.route";
+import placesRoutes from "./places.route";
 import "regenerator-runtime/runtime";
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -9,5 +10,7 @@ router.get("/health-check", (req, res) => res.send("OK"));
 
 // mount user routes at /users
 router.use("/users", userRoutes);
+
+router.use("/places", placesRoutes);
 
 export default router;
