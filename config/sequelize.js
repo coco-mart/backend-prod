0e47df27b62dda80e14841703d05cf27dc72535f
Pavithran Rajkumar <pavithranr.16cse@gmail.com>
2021-04-10 23:50:53 +0530

    Dialect options change

--- config/sequelize.js
+++ config/sequelize.js
@@ -18,5 +18,8 @@ module.exports = {
     production: {
         url: process.env.DATABASE_URL,
         dialect: "postgres",
+        dialectOptions: {
+            ssl: true,
+        },
     },
 };
